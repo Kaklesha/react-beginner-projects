866d47a89fc717d440e30ee2ef764b1c3f5ff654
kira <k-korni1ov@yandex.ru>
2023-07-13 17:05:30 +0400

    feat: done photo

--- src/App.js
+++ src/App.js
@@ -1,39 +1,54 @@
 import React from 'react';
+import {Collection }from './Collections.jsx';
+import data from './data.json'
 import './index.scss';
-import {Collection} from './Collections';
-
 
 function App() {
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [page, setPage] = React.useState(1);
+  const [searchValue, setSearchValue] = React.useState('');
+  const [collections, setCollections] = React.useState(data.collections[page-1]);
 
-  
+  React.useEffect(() => {
+    if (categoryId == 0){
+      setCollections(data.collections[page-1])
+    }
+    else{
+      setCollections(data.collections[page-1].filter(obj => obj.category == categoryId))
+    }
+  }, [categoryId, page])
+
   return (
     <div className="App">
       <h1>Моя коллекция фотографий</h1>
       <div className="top">
         <ul className="tags">
-          <li className="active">Все</li>
-          <li>Горы</li>
-          <li>Море</li>
-          <li>Архитектура</li>
-          <li>Города</li>
+          {
+            data.categories.map((obj, i) => <li onClick={() => setCategoryId(i)} className={categoryId == i ? "active" : ''} key={obj.name}>{obj.name}</li>)
+          }
         </ul>
-        <input className="search-input" placeholder="Поиск по названию" />
-      </div>
-      <div className="content">
-        <Collection
-          name="Путешествие по миру"
-          images={[
-            'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-          ]}
+        <input 
+          value={searchValue} 
+          onChange={e => setSearchValue(e.target.value)} 
+          className="search-input" 
+          placeholder="Поиск по названию"
         />
       </div>
+      <div className="content">
+        {collections.filter(obj => obj.name.toLowerCase().includes(searchValue.toLowerCase())).map((obj, index) => (
+          <Collection
+            key={index}
+            name={obj.name}
+            images={obj.photos}
+          />
+        ))
+
+        }
+      </div>
       <ul className="pagination">
-        <li>1</li>
-        <li className="active">2</li>
-        <li>3</li>
+        {[...Array(3)].map((_, i) => (
+          <li onClick={() => setPage(i + 1)} className={page == i + 1 ? "active" : ''}>{i + 1}</li>
+        ))}
       </ul>
     </div>
   );
