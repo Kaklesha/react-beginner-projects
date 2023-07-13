97b9c4e7a911b4c1ca6fa15aa3551f21173dea27
kira <k-korni1ov@yandex.ru>
2023-07-13 14:48:26 +0400

    feat: add/remove users of list

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
   const[users,setUsers]= React.useState([]);
   const[isLoading,setLoading]= React.useState(true);
   const[searchValue,setSearchValue]= React.useState('');
+  const[invites,setInvites]= React.useState([3,1]);
 
   React.useEffect(()=>{
     fetch('https://reqres.in/api/users')
@@ -27,13 +28,24 @@ const onChangeSearchValue=(e)=>{
   setSearchValue(e.target.value);
 }
 
+const onClickInvite=(id)=>{
+  if(invites.includes(id)){
+    setInvites(prev=>prev.filter(_id=>_id!==id));
+  } else {
+    setInvites(prev=>[...prev,id]);
+  }
+}
+
   return (
     <div className="App">
       <Users 
       onChangeSearchValue={onChangeSearchValue}
       searchValue={searchValue}
        items={users} 
-       isLoading={isLoading}/>
+       isLoading={isLoading}
+       invites={invites}
+       onClickInvite={onClickInvite}
+       />
       {/* <Success /> */}
     </div>
   );
--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Skeleton } from './Skeleton';
 import { User } from './User';
 
-export const Users = ({ items, isLoading, searchValue ,onChangeSearchValue}) => {
+export const Users = ({ items, isLoading, searchValue ,onChangeSearchValue, invites,onClickInvite}) => {
 
   console.log(searchValue)
 
@@ -34,6 +34,8 @@ export const Users = ({ items, isLoading, searchValue ,onChangeSearchValue}) => 
              
             }).map(obj=>(
               <User 
+              onClickInvite={onClickInvite}
+              isInvited={invites.includes(obj.id)}
               key={obj.id}
               {...obj}
               />
