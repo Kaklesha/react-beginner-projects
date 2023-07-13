7da6b6beb52bb5ab4c4bdb64576f15a37f9f87d0
kira <k-korni1ov@yandex.ru>
2023-07-13 12:49:55 +0400

    feat: 4th MW  with dynamic content

--- src/App.js
+++ src/App.js
@@ -5,20 +5,24 @@ function App() {
 
   const [open, setOpen] = React.useState(false);
 
-  const Model =({open,setOpen})=>(<div className={`overlay animated ${open&&'show'}`}>
+  const Model =({open,setOpen, children})=>(<div className={`overlay animated ${open&&'show'}`}>
   <div className="modal">
     <svg onClick={()=>setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
       <title />
       <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
     </svg>
-    <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+    {children}
   </div>
 </div> )
 
   return (
     <div className="App">
       <button onClick={()=>setOpen(true)} className="open-modal-btn">✨ Открыть окно</button>
-          {open&&<Model open={open} setOpen={setOpen}/>}
+          <Model open={open} setOpen={setOpen}>
+          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" alt="" />
+          <h3>This is modal window</h3>
+          <button>1231</button>
+          </Model>
     </div>
 
   );
