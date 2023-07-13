c232729d7e59e598288a9f452c3aa38e37d5a835
kira <k-korni1ov@yandex.ru>
2023-07-13 15:38:07 +0400

    fear: add rotation

--- src/App.js
+++ src/App.js
@@ -3,10 +3,28 @@ import { Block } from './Block';
 import './index.scss';
 
 function App() {
+  const [fromCurrency,setFromCurrency]=React.useState('RUS') 
+  const [toCurrency,setToCurrency]=React.useState('USD') 
+const [rates, setRates] = React.useState({});
+
+React.useEffect(()=>{
+  fetch('https://www.cbr-xml-daily.ru/latest.js')
+  .then((res)=>res.json())
+  .then((json)=>{
+    setRates(json.rates);
+    console.log(json.rates);
+  })
+  .catch((err)=>{
+    console.warn(err);
+    alert("Don't get info")
+  });
+},[]);
+
   return (
     <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
+      <Block value={0} currency={fromCurrency} onChangeCurrency={setFromCurrency} />
+      <Block value={0} currency={toCurrency} onChangeCurrency={setToCurrency} />
+     
     </div>
   );
 }
